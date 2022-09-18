4a628fb126e879d84baa2f141d03691619eafac5
gobinath shanthakumar <gobinathshanthakumar@gobinaths-MacBook-Air.local>
2022-09-18 11:15:27 -0400

    finish auth and username for chat

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import User, Chatroom, Message, Join_table
+from .models import Chatroom, Message
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Chatroom)
 admin.site.register(Message)
-admin.site.register(Join_table)
