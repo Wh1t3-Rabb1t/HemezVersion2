5938eb2b21aad190286b24b3e5ae54e1845a5735
Louis Cheung <louis.cheung.2@gmail.com>
2022-09-19 21:37:02 -0400

    update User and Profile Models works now

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 
 from django.contrib.auth.forms import UserCreationForm
 
 from django.db import transaction
-from .models import User, Profile
 # Importing instances of ModelForms
 from .forms import UserForm, ProfileForm
 
@@ -14,7 +13,7 @@ from django.contrib.auth.decorators import login_required
 #  for class based authorization
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# for chat 
+# for chat
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
@@ -24,76 +23,64 @@ from asgiref.sync import async_to_sync
 def lobby(request):
     return render(request, 'lobby.html')
 
+
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def chatrooms(request):
-  return render(request, 'chatrooms.html')
+    return render(request, 'chatrooms.html')
 
+@login_required
 def profile(request):
-  user = request.user
-  user_form = UserForm()
-  profile_form = ProfileForm()
-  return render(request, 'profile.html', {
-    'user': user,
-    'user_form': user_form,
-    'profile_form': profile_form
+    user = request.user
+    user_form = UserForm()
+    profile_form = ProfileForm()
+    return render(request, 'profile.html', {
+        'user': user,
+        'user_form': user_form,
+        'profile_form': profile_form
     })
 
+@login_required
 @transaction.atomic
 def user_update(request, user_id):
-  if request.method == "POST":
-    user_form = UserForm(request.POST)
-    profile_form = ProfileForm(request.POST)
-    
-    print('-----------------------', profile_form)
-    
-    # obj=Profile.objects.get(user_id=request.user.id)
-    if user_form.is_valid() and profile_form.is_valid():
-      # obj.bio = profile_form.cleaned_data['bio']
-      # u_update = user_form.save(commit=False)
-      # u_update.user_id = user_id
-      user_form.save()
-      # profile = profile_form.save(commit=False)
-      # profile.user = user
-      profile_form.save()
-      print('-----------------------------------')
-      
-      return redirect('profile')
-    else:
-      print(user_form.errors)
-      print(profile_form.errors)
-  return render(request, '')
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+        else:
+            print(user_form.errors)
+            print(profile_form.errors)
+    return render(request, '')
 
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('/')
-    else:
-      print(form.errors)
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
-
-
-
-
-
-
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('/')
+        else:
+            print(form.errors)
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 
 # CHAT FEATURES CHANNELS
@@ -107,7 +94,6 @@ class ChatConsumer(WebsocketConsumer):
         )
 
         self.accept()
-   
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -117,9 +103,9 @@ class ChatConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type':'chat_message',
-                'message':message,
-                'user':user
+                'type': 'chat_message',
+                'message': message,
+                'user': user
             }
         )
 
@@ -128,8 +114,8 @@ class ChatConsumer(WebsocketConsumer):
         user = event['user']
 
         self.send(text_data=json.dumps({
-            'type':'chat',
-            'message':message,
-            'user':user,
+            'type': 'chat',
+            'message': message,
+            'user': user,
 
         }))
